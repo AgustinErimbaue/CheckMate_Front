4c3c4020c83a853973b965c1e9b364e2cbff779d
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-05-29 19:03:25 +0200

    feat: refactor Header component structure and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <>
+    <header className={styles.header}>
       <h1>Check Mate</h1>
-      <ul>
+      <nav className={styles.nav}>
         <Link to="/">Tareas</Link>
         <Link to="/Login">Login</Link>
         <Link to="/Register">Registrarse</Link>
-      </ul>
-    </>
+      </nav>
+    </header>
   );
 };
 
