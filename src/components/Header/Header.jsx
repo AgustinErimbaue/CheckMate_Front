f2da77175754107c7d3b237ad1bc48bff3acf2c0
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-02 11:33:58 +0200

    feat: actualizar el componente Header para mostrar enlaces de inicio de sesión y registro según el estado del usuario

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
-import logo from "../../assets/imagen-app.png"; 
+import logo from "../../assets/imagen-app.png";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img src={logo} alt="Check Mate Logo" className={styles.logo} />
       </div>
       <nav className={styles.nav}>
-        <Link to="/">Tareas</Link>
-        <Link to="/Login">Login</Link>
-        <Link to="/Register">Registrarse</Link>
+        {!user ?(
+          <>
+            <Link to="/Login">Iniciar Sesión</Link>
+            <Link to="/Register">Registrarse</Link>
+          </>
+        ): (<Link to="/">Tareas</Link>)}
       </nav>
     </header>
   );
